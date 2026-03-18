887b568235a005eaadec6d0b3a1b9af3a92c8a4e
ryuleo-dev <ryuleo.dev@gmail.com>
2026-03-18 22:33:38 +0900

    chore: smalle fix

--- app/actions/trips.ts
+++ app/actions/trips.ts
@@ -4,7 +4,6 @@ import { createTrip, deleteTrip } from "@/lib/trip";
 import { revalidatePath } from "next/cache";
 
 export async function addTripAction(title: string, destination: string, startDate: string, endDate: string) {
-    const id = crypto.randomUUID();
     const trip = await createTrip(title, destination, startDate, endDate);
     revalidatePath("/trips");
     return trip;
